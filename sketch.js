e92ee4c8fd8830793e54a50dee4e0d143424d227
vibhor1107 <65174758+vibhor1107@users.noreply.github.com>
2020-06-26 13:13:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,55 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world; 
+ 
+   ground1 = new Ground(400,200,200,20)
+   ground2= new Ground(400,400,800,40)
+   box1 = new Box(400,150,20,20)
+  box2 = new Box(420,150,20,20)
+  box3 = new Box(440,150,20,20)
+  box4= new Box(380,150,20,20)
+  box5 = new Box(360,150,20,20)
+  box6=new Box(380,130,20,20)
+  box7= new Box(400,130,20,20)
+  box8= new Box(420,130,20,20)
+  box9 = new Box(400,130,20,20)
+
+polygon=new Pologon(50,200,40,6);
+  slingShot = new SlingShot(polygon.body,{x:100,y:200})
+}
+
+function draw() {
+  background(0,0,0);  
+  Engine.update(engine);
+  ground1.display();
+  ground2.display();
+  box1.display();
+  box2.display();
+  box3.display();
+  box4.display();
+  box5.display();
+  box6.display();
+  box7.display();
+  box8.display();
+  box9.display();
+  polygon.display();
+  slingShot.display();
+  drawSprites();
+}
+
+function mouseDragged(){
+        Matter.Body.setPosition(polygon.body, {x: mouseX , y: mouseY});  
+}
+
+
+function mouseReleased(){
+    slingShot.fly();
+}
